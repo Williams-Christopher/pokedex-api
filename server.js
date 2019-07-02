c473c114e8d75cf3e1d04cbb6321de447ab6b321
Christopher Williams <cw@chriswillia.ms>
2019-07-02 14:21:14 -0500

    Updates the project for deployment to Heroku.

--- server.js
+++ server.js
@@ -1,23 +1,36 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
 const POKEDEX = require('./pokedex.json');
 
 const app = express();
+const PORT = process.env.PORT || 8000;
 
-app.use(morgan('common'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting));
+app.use(cors());
+app.use(helmet());
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN;
     const authToken = req.get('Authorization');
 
-    console.log('validateBearerToken middleware...');
-
     if(!authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({error: 'Unauthorized request'});
     }
-
     // move to the next middleware
     next();
+});
+// Error handler
+app.use((error, req, res, next) => {
+    let response
+    if(process.env.NODE_ENV === 'production') {
+        response = {error: {message: 'Server error'}};
+    } else {
+        response = {error};
+    }
+    res.status(500).json(response);
 });
 
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
@@ -58,8 +71,8 @@ function handleGetPokemon(req, res) {
     res.json(results);
 }
 
-const PORT = 8000;
-
 app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}/`);
+    if(process.env.NODE_ENV !== 'production') {
+        console.log(`Server listening at http://localhost:${PORT}/`);
+    }
 });
