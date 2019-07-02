d3c1aacd142a69967ba46ad14dea85921709e058
Christopher Williams <cw@chriswillia.ms>
2019-07-01 20:12:13 -0500

    Implements searching by name and filtering by type, returning a JSON formatted response.

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const POKEDEX = require('./pokedex.json');
 
-console.log(process.env.API_TOKEN);
 const app = express();
 
 app.use(morgan('common'));
-
-const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
-
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN;
     const authToken = req.get('Authorization');
@@ -23,17 +20,43 @@ app.use(function validateBearerToken(req, res, next) {
     next();
 });
 
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
+
+app.get('/pokemon', handleGetPokemon);
+
+app.get('/types', handleGetTypes);
+
 function handleGetTypes(req, res) {
     res.json(validTypes);
 }
 
-app.get('/types', handleGetTypes);
-
 function handleGetPokemon(req, res) {
-    res.send('Hello, Pokemon!');
-}
+    let {name, type} = req.query;
+    
+    // If name isn't provided, set it to an empty string to match everything
+    if(!name) {
+        name = '';
+    }
 
-app.get('/pokemon', handleGetPokemon);
+    // make sure that the type parameter is valid if provided
+    if(type) {
+        //if(validTypes.filter(t => t.toLowerCase() === type.toLowerCase()) == false) {
+        if(!validTypes.includes(type)) {
+            return res.status(400).json({error: 'type parameter must be a valid type'});
+        }
+    }
+    
+    // find matching names
+    let results = POKEDEX.pokemon.filter(p => p.name.toLowerCase().includes(name.toLowerCase()));
+
+    // if provided, filter the results by type
+    if(type) {
+        //results = results.filter(p => p.type.filter(pt => pt.toLowerCase() === type.toLowerCase()) == true);
+        results = results.filter(p => p.type.includes(type));
+    }
+
+    res.json(results);
+}
 
 const PORT = 8000;
 
